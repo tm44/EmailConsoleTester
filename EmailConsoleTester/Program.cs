724634d24e8efec1bc88c28384a2540220d5f3a0
Mike Tate <shagee@uw.edu>
2023-01-30 13:33:08 -0800

    Create Email Tester

---
+++ EmailConsoleTester/Program.cs
@@ -0,0 +1,52 @@
+﻿using MailKit.Security;
+using MimeKit.Text;
+using MimeKit;
+using MailKit.Net.Smtp;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Configuration.Json;
+
+var builder = new ConfigurationBuilder();
+IConfiguration c = builder.AddJsonFile("appsettings.json")
+	.AddEnvironmentVariables()
+	.Build();
+
+var settings = c.GetRequiredSection("Settings").Get<Settings>();
+
+var email = new MimeMessage();
+email.From.Add(MailboxAddress.Parse(settings.From));
+
+foreach (var recip in settings.To.Split(','))
+{
+	email.To.Add(MailboxAddress.Parse(recip));
+}
+
+email.Subject = "Email Test";
+
+var file = @"c:\temp\test.html";
+string htmlSource = "";
+using (var stream = new StreamReader(file, System.Text.Encoding.ASCII, true))
+{
+	htmlSource = stream.ReadToEnd();
+}
+
+email.Body = new TextPart(TextFormat.Html) { Text = htmlSource };
+
+// send email
+using (var smtp = new SmtpClient())
+{
+	smtp.Connect(settings.SmtpHost, settings.Port, false);
+	smtp.Authenticate(settings.Username, settings.Password);
+	smtp.Send(email);
+	smtp.Disconnect(true);
+}
+
+public class Settings
+{
+	public string SmtpHost { get; set; } = string.Empty;
+	public string To { get; set; } = string.Empty;
+	public string From { get; set; } = string.Empty;
+
+	public string Username { get; set; } = string.Empty;
+	public string Password { get; set; } = string.Empty;
+	public int Port { get; set; } = 587;
+}
--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 # EmailConsoleTester
+
+## Steps to run
+Run an `npm install` to bring in necessary packages.
+
+Place the .html email template file in the C:\temp folder (or wherever, and adjust the code accordingly).
+
+Create a free SMTP account with [SendInBlue.com](https://sendinblue.com).  Once the account is set up, log in and go to the "Transactional" menu item.  Click on Settings from the left menu, then Configuration, then "Get Your SMTP key"
+
+Create an appsettings.json file that looks like the following:
+
+```
+  "Settings": {
+    "SmtpHost": "smtp-relay.sendinblue.com",
+    "Username": "YourUsername",
+    "Password": "YourPassword",
+    "To": "test@email.com",
+    "From": "your@email.com",
+    "Port": 587
+  }
+```
